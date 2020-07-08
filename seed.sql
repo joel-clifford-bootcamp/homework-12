0eca48070fc277191b28b78b6662d749fd4b98e7
Joel Clifford <joel.clifford@icloud.com>
2020-07-08 07:46:20 -0400

    added employees seeds

--- seed.sql
+++ seed.sql
@@ -1,5 +1,4 @@
-
-USE employee_tracker;
+use employee_tracker;
 
 INSERT INTO department (name) 
 VALUES 
@@ -17,3 +16,29 @@ VALUES
 	("Accountant",125000,1),
 	("Lawyer",190000,3),
 	("Legal Team Lead",250000,3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+	("Aleksandra", "Beach", 2, NULL),
+	("Cathy", "Barlow", 4, NULL),
+	("Adeeb", "Brennan", 7, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+	("Rose", "Mccray", 1, 1),
+	("Lorcan", "Hubbard", 1, 1),
+	("Soraya", "Iles", 1, 1),
+	("Alyce", "Rooney", 1, 1),
+	("Issac", "Lozano", 3, 6),
+	("Nylah", "Vincent", 3, 2),
+	("Rahim", "Rowe", 3, 2),
+	("Kenzo", "Nelson", 3, 2),
+	("Lidia", "Kidd", 5, 3),
+	("Graham", "Torres", 5, 3),
+	("Etta", "Hilton", 5, 3),
+	("Aysha", "Ayers", 6, 3),
+	("Gage", "Gross", 6, 3),
+	("Eli", "Parker", 6, 3),
+	("Israel", "Holmes", 3, 2),
+	("Ellen", "Hanson", 3, 2),
+	("Libby", "Mclaughlin", 3, 2)
