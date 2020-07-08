b630e757e16fd60d62e78aaf4defa7b89f6b2991
Joel Clifford <joel.clifford@icloud.com>
2020-07-08 07:47:10 -0400

    added mysql dependency

---
+++ con.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+
+module.exports = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bootcamp",
+    database:'employee_tracker'
+  });
+  
